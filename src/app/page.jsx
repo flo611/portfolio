f9263828efcf01d75117ab7fac8e19b7bb593f11
flo611 <florian.sabine911@outlook.fr>
2023-06-09 13:58:51 +0200

    nex title

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,8 +9,9 @@ export default function Home() {
       <p>
         Vous avez un <mark> besoin particulier </mark> pour créer votre site ?
         Venez nous rencontrer pour comprendre votre demande et répondre à vos
-        besoin. à vos téléphone
+        besoin. 
       </p>
+      <h2> Mes compétences </h2>
       <img
         src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.futura-sciences.com%2Ftech%2Fdefinitions%2Finformatique-reseau-informatique-599%2F&psig=AOvVaw0VZD2Rmu2LCR-dZVuHffIt&ust=1686386487304000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIDXu7rltf8CFQAAAAAdAAAAABAV"
         alt="Le réseau du dev"
