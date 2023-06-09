6bbf44bfc23bca6cc1e76dbe001c0ffd7163d813
flo611 <florian.sabine911@outlook.fr>
2023-06-09 10:50:10 +0200

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1> Bienvenue dans le monde du développement</h1>
+      <p> Vous avez un besoin particulier pour créer votre site ? Venez nous rencontrer pour comprendre votre demande et répondre à vos besoin</p>
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.futura-sciences.com%2Ftech%2Fdefinitions%2Finformatique-reseau-informatique-599%2F&psig=AOvVaw0VZD2Rmu2LCR-dZVuHffIt&ust=1686386487304000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCIDXu7rltf8CFQAAAAAdAAAAABAV" alt="Le réseau du dev" />  
+<a href="https://www.google.com/" target="_blank">Aller sur google</a>
+  </main>
+  )
+}
