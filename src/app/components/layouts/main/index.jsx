cd6bc592c44789a925132504747d944cc4a9753f
flo611 <florian.sabine911@outlook.fr>
2023-08-08 16:52:23 +0200

    update

--- src/app/components/layouts/main/index.jsx
+++ src/app/components/layouts/main/index.jsx
@@ -1,6 +1,6 @@
 const Main = ({ children }) => {
   return (
-    <main className="overflow-x-hidden bg-gradient-to-r from-cyan-300 to-cyan-950 text-white dark:bg-gradient-to-r dark:from-cyan-600 dark:to-purple-950 dark:text-white lg:text-2xl">
+    <main className="overflow-x-hidden bg-gradient-to-r from-cyan-400 to-cyan-950 text-white dark:bg-gradient-to-r dark:from-cyan-600 dark:to-purple-950 dark:text-white lg:text-2xl">
       {children}
     </main>
   );
