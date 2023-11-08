53d80a7b70549ae97a11e53720158b242b49dcbf
flo611 <florian.sabine911@outlook.fr>
2023-11-08 16:00:36 +0100

    udpate contactform

--- src/app/components/tools/form/index.jsx
+++ src/app/components/tools/form/index.jsx
@@ -1,136 +1,106 @@
-import React, { useEffect } from "react";
-
-import { Formik } from "formik";
-import Input from "@/app/components/tools/inputs";
-import Button from "@/app/components/tools/buttons";
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import emailjs from "@emailjs/browser";
+import "./styles.css"
+import Button from '../buttons';
 
-import "./styles.css";
+const ContactForm = () => {
+  const initialValues = {
+    name: '',
+    surname: '',
+    phone: '',
+    email: '',
+    message: '',
+  };
 
+  const onSubmit = (values, { resetForm }) => {
+    // Utilise email.js pour envoyer le formulaire par e-mail
+    emailjs.send('service_2up4qj8', 'template_qv435b9', values, 'okCH71BTXGKRxEAU6')
+      .then((response) => {
+        console.log('Email sent successfully:', response);
+        resetForm();
+        alert("formulaire envoyé");
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        alert("Une erreur s'est produite lors de l'envoi du formulaire.");
+      });
+  };
 
-emailjs;
-const onSubmit = (values) => {
-  const formElement = document.getElementById("contact-form");
-  emailjs
-    .sendForm("service_2up4qj8", "template_qv435b9", formElement)
-    .then(function (response) {
-      console.log("SUCCESS!", response);
-      alert("formulaire envoyé");
-    })
-    .catch(function (error) {
-      console.error("Erreur lors de l'envoi du formulaire:", error);
-      alert("Une erreur s'est produite lors de l'envoi du formulaire.");
-    });
-};
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Form>
+       <div className='contain my-12 font-assistantRegular flex flex-col mx-8 px-3 pt-6  md:mx-16 lg:my-0 lg:grid lg:grid-cols-2 lg:mx-96 lg:mb-20 hover:scale-100 hover:ease-in-out hover:duration-200 lg:hover:scale-105'>
+        <div className="flex flex-col items-center lg:ml-12">
+          <label htmlFor="name">Nom</label>
+          <Field
+         className="border border-blue-300 focus-visible:border-blue-300 focus:border-blue-300 rounded-lg bg-cyan-800 hover:transition hover:ease-in-out hover:duration-200 hover:bg-slate-100 text-black w-3/4 dark:hover:bg-cyan-950 "
+          type="text"
+          id="name"
+          name="name"
+        />
+        
+          <ErrorMessage name="name" component="div" />
+        </div>
 
-const Form = () => (
-  useEffect(() => {
-    emailjs.init("okCH71BTXGKRxEAU6");
-  }, []),
-  (
-    <div>
-      <Formik
-        initialValues={{ email: "", name: "", surname: "", phone: "",message:"" }} 
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form
-            id="contact-form"
-            className="contain my-12 font-assistantRegular flex flex-col mx-8 px-3 pt-6  md:mx-16 lg:my-0 lg:grid lg:grid-cols-2 lg:mx-96 lg:mb-20 hover:scale-100 hover:ease-in-out hover:duration-200 lg:hover:scale-105 "
-            onSubmit={handleSubmit}
-          >
-            <div className="flex flex-col items-center lg:ml-12 ">
-              <label htmlFor="name">Votre nom</label>
-              <Input
-                type="text"
-                name="name"
-                id="name"
-                className="w-3/4 dark:hover:bg-cyan-950"
-              />
-            </div>
+        <div className="flex flex-col items-center py-4 lg:pt-0 lg:mr-12">
+          <label htmlFor="surname">Prénom</label>
+          <Field
+           className="border border-blue-300 focus-visible:border-blue-300 focus:border-blue-300 rounded-lg bg-cyan-800 hover:transition hover:ease-in-out hover:duration-200 hover:bg-slate-100 text-black w-3/4 dark:hover:bg-cyan-950 "
+            type="text"
+            id="surname"
+            name="surname"
+          />
+          <ErrorMessage name="prenom" component="div" />
+        </div>
 
-            <div className="flex flex-col items-center py-4 lg:pt-0 lg:mr-12">
-              <label htmlFor="surname">Votre prénom</label>
-              <Input
-                type="text"
-                name="surname"
-                id="surname"
-                className="w-3/4 dark:hover:bg-cyan-950"
-              />
-            </div>
-            <div className="flex flex-col items-center lg:pt-4 lg:ml-12">
-              <label htmlFor="phone">Votre Téléphone</label>
-              <Input
-                type="text"
-                name="phone"
-                id="phone"
-                className="w-3/4 dark:hover:bg-cyan-950"
-              />
-            </div>
-            <div className="flex flex-col items-center py-4 lg:mr-12">
-             <label htmlFor="email"> Votre adresse email</label>
-              <Input
-                className="w-3/4  dark:hover:bg-cyan-950"
-                type="email"
-                name="email"
-                id="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-            </div>
-            {errors.email && touched.email && errors.email}
+        <div className="flex flex-col items-center lg:pt-4 lg:ml-12">
+          <label htmlFor="phone">Téléphone</label>
+          <Field
+            className="border border-blue-300 focus-visible:border-blue-300 focus:border-blue-300 rounded-lg bg-cyan-800 hover:transition hover:ease-in-out hover:duration-200 hover:bg-slate-100 text-black w-3/4 dark:hover:bg-cyan-950 "
+            type="text"
+            id="phone"
+            name="phone"
 
-            <div className="mx-8  flex flex-col items-center  lg:justify-center lg:col-span-2 lg:py-4 lg:mx-2">
-              <label htmlFor="message">Objet du message</label>
+          />
+          <ErrorMessage name="phone" component="div" />
+        </div>
 
-              <textarea
-                name="message"
-                id="message"
-                className="h-40 px-3 md:w-9/12 lg:w-10/12   lg:h-72 rounded-lg bg-cyan-800 hover:bg-white text-black border border-blue-300 focus-visible:border-blue-300 focus:border-blue-300 hover:transition hover:ease-in-out hover:duration-200 dark:hover:bg-cyan-950"
-              ></textarea>
-            </div>
-            <div className="flex flex-row justify-center py-8 lg:col-span-2 lg:py-8 lg:pb-10">
+        <div className="flex flex-col items-center py-4 lg:mr-12">
+          <label htmlFor="email">Email</label>
+          <Field  className="border border-blue-300 focus-visible:border-blue-300 focus:border-blue-300 rounded-lg bg-cyan-800 hover:transition hover:ease-in-out hover:duration-200 hover:bg-slate-100 text-black w-3/4 dark:hover:bg-cyan-950 "
+            type="email"
+            id="email"
+            name="email"
+
+          />
+          <ErrorMessage name="email" component="div" />
+        </div>
+
+        <div className="mx-8  flex flex-col items-center  lg:justify-center lg:col-span-2 lg:py-4 lg:mx-2">
+          <label htmlFor="message"> Objet du Message</label>
+          <Field
+            as="textarea"
+            id="message"
+            name="message"
+            className="h-40 px-3 md:w-9/12 lg:w-10/12   lg:h-72 rounded-lg bg-cyan-800 hover:bg-white text-black border border-blue-300 focus-visible:border-blue-300 focus:border-blue-300 hover:transition hover:ease-in-out hover:duration-200 dark:hover:bg-cyan-950"
+          />
+          <ErrorMessage name="message" component="div" />
+        </div>
+        <div className="flex flex-row justify-center py-8 lg:col-span-2 lg:py-8 lg:pb-10">
               <Button
                 type="submit"
-                disabled={isSubmitting}
                 value="Envoyer"
                 className="uppercase px-4 py-4 w-36"
                 onClick={() => onSubmit() }
               ></Button>
             </div>
-          </form>
-        )}
-      </Formik>
-    </div>
-  )
-  );
-  
 
-export default Form;
+        </div>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
+﻿
