1be8054d28cb778dae4159247c89c9f0f08b7e7f
flo611 <florian.sabine911@outlook.fr>
2024-02-06 15:23:17 +0100

    update smiley

--- src/app/home/section/introduction/index.jsx
+++ src/app/home/section/introduction/index.jsx
@@ -37,7 +37,7 @@ const Introduction = () => {
             <TypeAnimations />
             <p className="pt-4 ">
               Je suis passioné de sports,de nature et de voyages. Si vous voulez
-              en savoir plus vous êtes au bon endroit!
+              en savoir plus vous êtes au bon endroit!😊
             </p>
           </div>
           <div className="flex flex-row justify-center pb-26 lg:pb-24 ">
